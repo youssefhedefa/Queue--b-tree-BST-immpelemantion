ada4342ce55e2a11141c5d3e91ab45967e551b29
youssefhedefa <78417605+youssefhedefa@users.noreply.github.com>
2022-05-07 16:00:37 +0200

    Add files via upload

---
+++ call center/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+
+namespace call_center
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue myQueue = new Queue();
+
+            Console.Write("Enter number of customers : ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter the name of customer number {0}  : ",(i+1));
+                string name = Console.ReadLine();
+                myQueue.Enqueue(name);
+            }
+
+            Console.WriteLine("Next one is {0}", myQueue.Dequeue());
+            Console.WriteLine("You still have {0} customer ", myQueue.Count);
+            Console.Write("Do you need more {Y for yes , any key for for no} ? ");
+            char answer = char.Parse(Console.ReadLine().ToUpper());
+
+            if(answer == 'Y')
+            {
+                while(myQueue.Count > 0)
+                {
+                    Console.WriteLine("You still have {0} customer ", myQueue.Count);
+                    Console.WriteLine("Next one is {0}", myQueue.Dequeue());
+
+                    Console.Write("Do you need more {Y for yes , any key for for no} ? ");
+                    char answer2 = char.Parse(Console.ReadLine().ToUpper());
+
+                    if (answer2 != 'Y')
+                    {
+                        break;
+                    }
+                }
+
+            }
+            Console.WriteLine("Take a break");
+        }
+    }
+}
